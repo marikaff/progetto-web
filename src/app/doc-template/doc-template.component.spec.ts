4c5ca2d8ec7c5839a8992c8ec4256409ebe0467f
marikaff <marikafoti2017@gmail.com>
2025-01-21 21:17:58 +0100

    COMMIT

---
+++ src/app/doc-template/doc-template.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DocTemplateComponent } from './doc-template.component';
+
+describe('DocTemplateComponent', () => {
+  let component: DocTemplateComponent;
+  let fixture: ComponentFixture<DocTemplateComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [DocTemplateComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(DocTemplateComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/doc-template/doc-template.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-doc-template',
+  imports: [],
+  templateUrl: './doc-template.component.html',
+  styleUrl: './doc-template.component.css'
+})
+export class DocTemplateComponent {
+
+}
