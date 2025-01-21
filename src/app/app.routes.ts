16dddb26580e484e19decbde13099184d8ab4358
marikaff <marikafoti2017@gmail.com>
2025-01-21 21:46:48 +0100

    LEZGOSKI

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,8 +3,13 @@ import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
 import { LoginComponent } from './login/login.component';
 import { RegistrationComponent } from './registration/registration.component';
+import { FooterComponent } from './footer/footer.component';
+
 //import { PreferitiComponent } from './preferiti/preferiti.component';
-//
+//import { AdminComponent } from './admin/admin.component';
+//import { DocTemplateComponent } from './doc-template/doc-template.component';
+
+//QUANDO SI CREANO LE PAGINE TOGLIERE COMMENTI A MANO A MANO
 
 
 export const routes: Routes = [
@@ -12,7 +17,10 @@ export const routes: Routes = [
     {path: 'header', component: HeaderComponent},
     {path: 'login', component: LoginComponent},
     {path: 'registration', component: RegistrationComponent},
-    //{path: 'preferiti', component: PreferitiComponent}
+    {path: 'footer', component: FooterComponent},
+    //{path: 'preferiti', component: PreferitiComponent},
+    //{path: 'admin', component: AdminComponent},
+    //{path: 'docTemplate', component:DocTemplateComponent},
     {path: '', redirectTo:'/home', pathMatch: 'full'}
 
 ];
--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,14 @@ import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { LoginComponent } from './app/login/login.component';
 import { HomeComponent } from './app/home/home.component';
+
+//import { HeaderComponent } from './header/header.component';
+//import { RegistrationComponent } from './registration/registration.component';
+//import { FooterComponent } from './footer/footer.component';
 //import { ProfiloComponent } from './app/profilo/profilo.component';
 //import { PreferitiComponent } from './app/preferiti/preferiti.component';
+//import { AdminComponent } from './admin/admin.component';
+//import { DocTemplateComponent } from './doc-template/doc-template.component';
 
 
 
